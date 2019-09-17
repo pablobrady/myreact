975e618c3454e3fd9f93015ecfb3942ab27eb539
PB <pablo.brady@gmail.com>
2019-09-16 22:04:11 -0700

    Updates

---
+++ precache-manifest.e3cf60d53038b9e7575c2e799faf614e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e20dc353704aaa49ba7f498255f985",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "4d009cb40457ec4d94b1",
+    "url": "/myreact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "aaa132b1d53febff999d",
+    "url": "/myreact/static/js/2.37d2f18b.chunk.js"
+  },
+  {
+    "revision": "4d009cb40457ec4d94b1",
+    "url": "/myreact/static/js/main.3e44037c.chunk.js"
+  },
+  {
+    "revision": "bf3e660c46fd42caf2ac",
+    "url": "/myreact/static/js/runtime~main.2b310166.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
